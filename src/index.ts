3bc87a7ce778650d45f7a482e073a0b61fb3cfbd
Ian Hornik <yiliansource@gmail.com>
2021-05-04 16:06:38 +0200

    Added type declarations to export

--- src/index.ts
+++ src/index.ts
@@ -10,6 +10,13 @@ export const time = new TimeEndpoint();
 export const pastes = new PasteEndpoint();
 export const users = new UserEndpoint();
 
+export * from "./types/edit";
+export * from "./types/expiresIn";
+export * from "./types/language";
+export * from "./types/paste";
+export * from "./types/pasty";
+export * from "./types/user";
+
 /**
  * Provides an API token to the library, used to authorize private requests.
  *
